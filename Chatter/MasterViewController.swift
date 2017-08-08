aae143960c1254269fd20aa94a7b10f885c13dbe
iD Student <idstudent@dhcp-18-111-78-43.dyn.mit.edu>
2017-08-08 15:07:43 -0400

    3:10 PM tuesday

--- Chatter/MasterViewController.swift
+++ Chatter/MasterViewController.swift
@@ -8,76 +8,91 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController
-{
-
+class MasterViewController: UITableViewController {
+    
     var detailViewController: DetailViewController? = nil
     var objects = [Any]()
-
-
+    
+    
+    
+    @IBAction func cancelButtonToMainMenu (segue: UIStoryboardSegue)
+    {
+        
+    }
+    @IBAction func doneButtonToSaveNewPost (segue: UIStoryboardSegue)
+    {
+        
+    }
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @objc
     func insertNewObject(_ sender: Any) {
         objects.insert(NSDate(), at: 0)
         let indexPath = IndexPath(row: 0, section: 0)
         tableView.insertRows(at: [indexPath], with: .automatic)
     }
-
+    
     // MARK: - Segues
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
-                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
-                controller.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        /*  if segue.identifier == "showDetail" {
+         if let indexPath = tableView.indexPathForSelectedRow {
+         let object = objects[indexPath.row] as! NSDate
+         let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+         controller.detailItem = object
+         controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+         controller.navigationItem.leftItemsSupplementBackButton = true
+         }
+         } */
     }
-
+    
+    
     // MARK: - Table View
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
+    var posts = postArray
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return posts.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+        
+        let posts = self.posts[indexPath.row] as! Post
+        cell.textLabel!.text = posts.text
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
     {
-
+        
     }
 }
+
 
---
+++ Chatter/Post.swift
@@ -0,0 +1,30 @@
+//
+//  Post.swift
+//  Chatter
+//
+//  Created by iD Student on 8/8/17.
+//  Copyright © 2017 BohanMa. All rights reserved.
+//
+
+import Foundation
+
+class Post
+{
+    var text: String
+    var date: NSDate
+    var userName: String
+    
+    init(text: String, date: NSDate, userName: String)
+    {
+        self.text = text
+        self.date = date
+        self.userName = userName
+    }
+}
+
+let postArray : [Post] =
+    [
+        Post(text: "life is short   ", date: NSDate(), userName: "BohanMa2002" ),
+        Post(text: "so just yolo it!", date: NSDate(), userName: "BohanMa2002" )
+]
+
